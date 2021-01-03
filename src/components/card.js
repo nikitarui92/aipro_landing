10832dd79f26fc06fd8e6cafc16f0365f5dfeec8
Nikita <nikitarui92@gmail.com>
2021-01-03 12:21:31 +0300

    add cards

---
+++ src/components/card.js
@@ -0,0 +1,51 @@
+
+
+import { html } from 'lit-element';
+
+import { BaseElement } from '../base-element'
+
+class Card extends BaseElement {
+
+    static get properties() {
+        return {
+            img: {type: String},
+            desc: {type: String},
+            name: {type: String},
+            link: {type: String}
+        }
+    }
+
+    constructor(){
+        super()
+        this.img = ''
+        this.desc = ''
+        this.fullname = ''
+        this.link = ''
+    }
+
+    render() {
+        return html`
+        <div class="box">
+            <article class="media">
+                <div class="media-left">
+                    <figure class="image is-64x64">
+                        <img src="${this.img}" alt="Card">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>${this.fullname}</strong> <small>${this.link}</small>
+                            <br>
+                            ${this.desc}
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    `;
+    }
+
+}
+
+customElements.define('l-card', Card);
--- src/components/contact.js
+++ src/components/contact.js
@@ -3,6 +3,8 @@ import { html, unsafeCSS } from 'lit-element';
 import { BaseElement } from '../base-element'
 import styles from './contact.scss'
 
+import './card'
+
 class Contact extends BaseElement {
 
     static get styles() {
@@ -23,31 +25,11 @@ class Contact extends BaseElement {
                 <div class="container">
                     <div class="columns">
                         <div class="column">
-                            <div class="card">
-                                <div class="card-image">
-                                    <figure class="image is-4by3">
-                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                                    </figure>
-                                </div>
-                                <div class="card-content">
-                                    <div class="media">
-                                        <div class="media-content">
-                                            <p class="title is-4">Никита Захаров</p>
-                                            <p class="subtitle is-6">@nikitarui</p>
-                                        </div>
-                                    </div>
-        
-                                    <div class="content">
-                                        Гомогенная среда стабилизирует гидродинамический удар даже в случае сильных
-                                        локальных
-                                        возмущений среды.
-                                    </div>
-                                </div>
-                            </div>
+                            <l-card img="./img/max-card.jpg"></l-card>
                         </div>
-                        <div class="column"></div>
-                        <div class="column"></div>
-                        <div class="column"></div>
+                        <div class="column">
+                            <l-card img="./img/niki-card.jpg"></l-card>
+                        </div>
                     </div>
                 </div>
             </div>
