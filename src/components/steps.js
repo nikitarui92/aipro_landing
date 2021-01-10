6d2f6f67ea6df5d17720ef27509e1bc6a4b8f4fc
Nikita <nikitarui92@gmail.com>
2021-01-10 14:31:44 +0300

    add logo to steps

--- src/components/steps.js
+++ src/components/steps.js
@@ -18,21 +18,33 @@ class Steps extends BaseElement {
                 <div class="container">
                     <ul class="steps has-content-centered">
                         <li class="steps-segment is-active">
-                            <span class="steps-marker is-warning"></span>
+                            <span class="steps-marker is-warning">
+                                <span class="icon">
+                                    <i class="fas fa-rocket"></i>
+                                </span>
+                            </span>
                             <div class="steps-content">
                                 <p class="is-size-4">Постановка гипотезы</p>
                                 <p>Вы приходите к нам с проблемой, мы вместе находим решение</p>
                             </div>
                         </li>
                         <li class="steps-segment is-active">
-                            <span class="steps-marker"></span>
+                            <span class="steps-marker">
+                                <span class="icon">
+                                    <i class="fas fa-database"></i>
+                                </span>
+                            </span>
                             <div class="steps-content">
                                 <p class="is-size-4">Сбор и анализ данных</p>
                                 <p>Мы используем открытые данные или собираем свои </p>
                             </div>
                         </li>
                         <li class="steps-segment is-active">
-                            <span class="steps-marker"></span>
+                            <span class="steps-marker">
+                                <span class="icon">
+                                    <i class="fas fa-laptop-code"></i>
+                                </span>
+                            </span>
                             <div class="steps-content">
                                 <p class="is-size-4">Обучение модели</p>
                                 <p>На этом этапе происходит самое главное: мы обучаем модель, которая в дальнейшем будет
@@ -40,7 +52,11 @@ class Steps extends BaseElement {
                             </div>
                         </li>
                         <li class="steps-segment is-active">
-                            <span class="steps-marker"></span>
+                            <span class="steps-marker">
+                                <span class="icon">
+                                    <i class="fa fa-check"></i>
+                                </span>
+                            </span>
                             <div class="steps-content">
                                 <p class="is-size-4">Получение и применение модели</p>
                                 <p>Вы получаете доступ к модели или удобному API. Мы продолжаем вести ваш проект и постоянно
@@ -49,11 +65,6 @@ class Steps extends BaseElement {
                         </li>
                     </ul>
                 </div>
-                <!-- <div class="column has-text-right">
-                    <h1 class="title has-text-white">
-                        Wbfnnf
-                    </h1>
-                </div> -->
             </div>
         </section>
     `;
